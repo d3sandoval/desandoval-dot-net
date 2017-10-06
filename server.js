cd31b64f8a20ace47d70c275473b05e79e46700a
Daniel Sandoval <daniel@discuss.io>
2017-10-05 20:45:34 -0700

    merged with styling branch

--- server.js
+++ server.js
@@ -5,27 +5,26 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// see: https://github.com/zeit/next.js#custom-server-and-routing
-app.prepare()
+app.prepare();
 .then(() => {
-  const server = express();
+  const server = express()
 
   server.get('/p/:id', (req, res) => {
-    const actualPage = '/post';
-    const queryParams = { title: req.params.id };
-    app.render(req, res, actualPage, queryParams);
+    const actualPage = '/post'
+    const queryParams = { title: req.params.id } 
+    app.render(req, res, actualPage, queryParams)
   });
 
   server.get('*', (req, res) => {
-    return handle(req, res);
+    return handle(req, res)
   });
 
   server.listen(3000, (err) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000')
-  });
+  })
 })
-.catch((ex => {
+.catch((ex) => {
   console.error(ex.stack);
-  process.exit(1);
-}));
+  process.exit(1)
+});
