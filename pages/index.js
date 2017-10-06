ace04061f9d2d83e35957aa86030a3e861776751
Daniel Sandoval <daniel@discuss.io>
2017-10-05 20:38:21 -0700

    batman fail

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
+// const PostLink = (props) => (
+//   <li>
+//     <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//       <a>{props.title}</a>
+//     </Link>
+//   </li>
+// );
+
+const Index = (props) => (
 	<Layout>
-    <p>Hello Next.js</p>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  }
+};
 
 export default Index;
