36ad02a8d821cceac87291aec352089987b7345a
Daniel Sandoval <daniel@discuss.io>
2017-10-14 23:22:57 -0700

    refactored contact row

--- components/ContactButton.js
+++ components/ContactButton.js
@@ -5,11 +5,10 @@ import { CSSTransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
+import Grow from 'material-ui/transitions/Grow';
+import ContactRow from './ContactRow';
 import QuestionAnswerIcon from 'material-ui-icons/QuestionAnswer';
 import CloseIcon from 'material-ui-icons/Close';
-import EmailIcon from 'material-ui-icons/Email';
-import PhoneIcon from 'material-ui-icons/Phone';
-import CalendarIcon from 'material-ui-icons/Event';
 
 const duration = 500
 const styles = theme => ({
@@ -18,45 +17,6 @@ const styles = theme => ({
     position: 'fixed',
     bottom: 16,
     right: 16,
-    zIndex: 100,
-  },
-  emailIcon: {
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shorter,
-      easing: theme.transitions.easing.easeOut,
-    }),
-    margin: theme.spacing.unit,
-    width: 40,
-    height: 40,
-    position: 'fixed',
-    bottom: 84,
-    right: 24,
-    zIndex: 100,
-  },
-  phoneIcon: {
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shorter,
-      easing: theme.transitions.easing.easeOut,
-    }),
-    margin: theme.spacing.unit,
-    width: 40,
-    height: 40,
-    position: 'fixed',
-    bottom: 136,
-    right: 24,
-    zIndex: 100,
-  },
-  calendarIcon: {
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shorter,
-      easing: theme.transitions.easing.easeOut,
-    }),
-    margin: theme.spacing.unit,
-    width: 40,
-    height: 40,
-    position: 'fixed',
-    bottom: 188,
-    right: 24,
     zIndex: 100,
   },
   animated: {
@@ -89,17 +49,7 @@ class ContactButton extends React.Component {
             : <QuestionAnswerIcon color="white" />}
           </Button>
         {(this.state.open)
-        ? (<span>
-            <a href="mailto:daniel@desandoval.net"><Button className={classes.emailIcon} fab color="white">
-              <EmailIcon dense={true} />
-            </Button></a>
-            <a href="tel:+15417198286"><Button className={classes.phoneIcon} fab color="white">
-                <PhoneIcon dense={true} />
-            </Button></a>
-            <a href="https://calendly.com/d3sandoval/30min" target="_blank" rel="noopener noreferrer"><Button className={classes.calendarIcon} fab color="white">
-              <CalendarIcon dense={true} />
-            </Button></a>
-          </span>)
+        ? (<span><ContactRow /></span>)
         : <span></span>}
       </div>
     );
---
+++ components/ContactRow.js
@@ -0,0 +1,76 @@
+// @flow weak
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import EmailIcon from 'material-ui-icons/Email';
+import PhoneIcon from 'material-ui-icons/Phone';
+import CalendarIcon from 'material-ui-icons/Event';
+
+const styles = theme => ({
+  root: {}, // Todo make this relative to the parent (instead of absolute)
+  emailIcon: {
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shorter,
+      easing: theme.transitions.easing.easeOut,
+    }),
+    margin: theme.spacing.unit,
+    width: 40,
+    height: 40,
+    position: 'fixed',
+    bottom: 84,
+    right: 24,
+    zIndex: 100,
+  },
+  phoneIcon: {
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shorter,
+      easing: theme.transitions.easing.easeOut,
+    }),
+    margin: theme.spacing.unit,
+    width: 40,
+    height: 40,
+    position: 'fixed',
+    bottom: 136,
+    right: 24,
+    zIndex: 100,
+  },
+  calendarIcon: {
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shorter,
+      easing: theme.transitions.easing.easeOut,
+    }),
+    margin: theme.spacing.unit,
+    width: 40,
+    height: 40,
+    position: 'fixed',
+    bottom: 188,
+    right: 24,
+    zIndex: 100,
+  },
+});
+
+function ContactRow(props) {
+  const {classes} = props;
+
+  return (
+    <div className={classes.root}>
+      <a href="mailto:daniel@desandoval.net"><Button className={classes.emailIcon} fab color="white">
+        <EmailIcon dense={true} />
+      </Button></a>
+      <a href="tel:+15417198286"><Button className={classes.phoneIcon} fab color="white">
+        <PhoneIcon dense={true} />
+      </Button></a>
+      <a href="https://calendly.com/d3sandoval/30min" target="_blank" rel="noopener noreferrer"><Button className={classes.calendarIcon} fab color="white">
+        <CalendarIcon dense={true} />
+      </Button></a>
+    </div>
+  );
+}
+
+ContactRow.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContactRow);
