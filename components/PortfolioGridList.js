6ddcfe605904d2c0910f954b7f2553aa0ced9cae
Daniel Sandoval <daniel@discuss.io>
2018-01-27 17:09:18 -0800

    upgraded a few dependencies and fixed a bug with material-ui gridlist changes

--- components/PortfolioGridList.js
+++ components/PortfolioGridList.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import { GridList, GridListTile, GridListTileBar } from 'material-ui/GridList';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import PortfolioIcon from './PortfolioIcon';
 
