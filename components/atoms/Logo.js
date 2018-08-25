33ea9654eb6ec0cf3d5a1d085074ffca93637109
Daniel Sandoval <daniel@discuss.io>
2018-08-24 21:23:47 -0400

    crappy WIP

--- components/atoms/Logo.js
+++ components/atoms/Logo.js
@@ -3,68 +3,75 @@ import { Animated } from 'react-web-animation';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-    before: {
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        cursor: 'pointer',
-        '&:hover': {
-            opacity: 0.8
-        }
+  before: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    cursor: 'pointer',
+    '&:hover': {
+      opacity: 0.8,
     },
-    after: {
-        position: 'fixed',
-        top: '50%',
-        left: '50%'
-    }
-})
+  },
+  after: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+  },
+});
 
 class Logo extends React.Component {
     state = {
-        hasAnimated: false,
-        playState: 'paused'
+      hasAnimated: false,
+      playState: 'paused',
     }
+
     getKeyFrames(size) {
-        return [
-            { transform: 'scale(1)',   opacity: 1,   offset: 0 },
-            { transform: 'scale(1)', opacity: 0.75, offset: 0.25 },
-            { transform: `scale(2) translateX(-${size/1.5}px) translateY(-${size/4}px)`, opacity: 0.45, offset: 1 }
-        ];
+      return [
+        { transform: 'scale(1)', opacity: 1, offset: 0 },
+        { transform: 'scale(1)', opacity: 0.75, offset: 0.25 },
+        { transform: `scale(2) translateX(-${size / 1.5}px) translateY(-${size / 4}px)`, opacity: 0.45, offset: 1 },
+      ];
     }
 
-    getTiming( duration ) {
-        return {
-            duration,
-            easing: 'ease-in-out',
-            delay: 0,
-            iterations: 1,
-            direction: 'alternate',
-            fill: 'forwards'
-        };
+    getTiming(duration) {
+      return {
+        duration,
+        easing: 'ease-in-out',
+        delay: 0,
+        iterations: 1,
+        direction: 'alternate',
+        fill: 'forwards',
+      };
     }
 
-    handleClick = () => {
-        if (!this.state.hasAnimated) {
-            this.setState({playState:'running', hasAnimated: true});
-        }
-        
+    componentDidUpdate = () => {
+      if (this.props.homeRoute && !this.state.hasAnimated) {
+        this.setState({ playState: 'running', hasAnimated: true });
+      }
     }
 
     render() {
-    const { classes, size } = this.props;
-    return (
+      const { classes, size } = this.props;
+      return (
         <React.Fragment>
-            <Animated.div keyframes={this.getKeyFrames(size)}
+          <Animated.div
+            keyframes={this.getKeyFrames(size)}
             timing={this.getTiming(2000)}
             playState={this.state.playState}
-            >
-            <img alt="Click to enter" onClick={(this.state.hasAnimated) ? "" : this.handleClick} width={size} height={size} src="/img/logo.png" className={(this.state.hasAnimated) ? classes.after : classes.before } style={{
-                marginTop: (size/4),
-                marginLeft: -(size/2),
-            }} />
-            </Animated.div>
+          >
+            <img
+              alt="logo for DESandoval.net"
+              width={size}
+              height={size}
+              src="/img/logo.png"
+              className={(this.state.hasAnimated) ? classes.after : classes.before}
+              style={{
+                marginLeft: -(size / 2),
+              }}
+            />
+          </Animated.div>
         </React.Fragment>
-    );
+      );
     }
 }
 
--- components/organisms/HomeHero.js
+++ components/organisms/HomeHero.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import Logo from '../atoms/Logo';
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '100vh',
+    overflow: 'hidden',
+  },
+  title: {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: 32,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  bottom: {
+    position: 'absolute',
+    bottom: 132,
+    width: '100%',
+    textAlign: 'center',
+  },
+});
+
+function getHeight() {
+  // todo make this sane
+  if (typeof (window) === 'undefined') {
+    return 500;
+  }
+  return window.innerHeight / 2;
+}
+
 class HomeHero extends React.Component {
+    state = {
+      homeRoute: null,
+    }
+
+    handleClick = () => {
+      window.location.hash = 'something';
+      this.setState({ homeRoute: 'something' });
+    }
+
     render() {
-        return(
-            <Logo size={500} />
-        )
+      const { classes } = this.props;
+      return (
+        <div
+          onClick={(this.state.homeRoute) ? '' : this.handleClick}
+          onKeyPress={(this.state.homeRoute) ? '' : this.handleClick}
+          role="button"
+          tabIndex="0"
+          className={classes.root}
+        >
+          {(this.state.homeRoute === null) ? (
+            <span>
+              <Typography className={classes.title} variant="display4" gutterBottom>Daniel E. Sandoval</Typography>
+              <Typography className={classes.bottom} variant="headline">Putting the human experience first. Developing solutions to make it better.</Typography>
+            </span>
+          ) : (<p>test</p>)}
+          <Logo size={getHeight()} homeRoute={this.state.homeRoute} />
+        </div>
+      );
     }
 }
 
-export default HomeHero;
+export default withStyles(styles)(HomeHero);
